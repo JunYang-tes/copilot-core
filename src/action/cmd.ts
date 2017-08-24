a395b6d1685858f79457ccf8312e8b1e6f31f514
Qianba <ouyangqianba@gmail.com>
2017-08-24 11:48:32 +0800

    fix: Dot kill cp after copilot exited

--- src/action/cmd.ts
+++ src/action/cmd.ts
@@ -3,7 +3,9 @@ import utils from "../util"
 const { debug, error } = require("b-logger")("copilot.action.cmd")
 export async function cmd(param: ICmdParam) {
   try {
-    await utils.exec(param.cmd, param.args)
+    await utils.exec(param.cmd, param.args, {
+      detached: true
+    })
   } catch (e) {
     error(`Failed to execute `, param)
     error(e)
