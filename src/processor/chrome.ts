b4cfe17695040cd98f26547c9ea2e4a75a2102b4
Qianba <ouyangqianba@gmail.com>
2017-08-26 19:08:38 +0800

    feat:Chrome operation

--- src/processor/chrome.ts
+++ src/processor/chrome.ts
@@ -12,6 +12,10 @@ interface ITab {
   id: number,
   winId: number
 }
+interface IBookmark {
+  title: string,
+  url: string
+}
 class ChromeProxy {
   private srpc: {
     call<T>(method: string, ...args): Promise<T>
@@ -20,16 +24,40 @@ class ChromeProxy {
     this.srpc = srpc
   }
   public getHistory(): Promise<IHistory> {
-    return this.srpc.call<IHistory>("getHistory")
+    return this.srpc.call<IHistory>("getHistory", { text: "" })
   }
   public getTabs(): Promise<ITab> {
     return this.srpc.call<ITab>("getTabs")
   }
-  public activeTab(id: number) {
-    return this.srpc.call("activeTab", id)
+  public activeTab = (wid: number, tid: number) => {
+    return this.srpc.call("activeTab", wid, tid)
+  }
+  public closeTab = (id: number) => {
+    return this.srpc.call("closeTab", id)
+  }
+  /**
+   * getCurrentTab
+   */
+  public getCurrentTab = (): Promise<ITab> => {
+    return this.srpc.call("getCurrentTab")
+  }
+  public newTab = (prop) => {
+    return this.srpc.call("newTab", prop)
+  }
+  public getBookmarks = (count): Promise<IBookmark> => {
+    return this.srpc.call("getBookmarks", count)
+  }
+  public notify = (title, message) => {
+    return this.srpc.call("notify", {
+      type: "basic", title, message,
+    })
+  }
+  public createWin = (prop) => {
+    return this.srpc.call("createWin", prop)
   }
 }
-const TIPS = "This means chrome not may running or extention may not be installed"
+const urlFix = (url) => /^http/.test(url) ? url : `http://${url}`
+const TIPS = "This means chrome not may running or extension may not be installed"
 export default decorate({
   declare() {
     return {
@@ -41,23 +69,50 @@ export default decorate({
       }
     }
   },
-  init({ services }) {
+  init({ services, maxBookmarksCount }) {
     let { srpc } = services
     this.srpc = srpc
     this.chrome = new ChromeProxy(srpc)
+    this.maxBookmarksCount = maxBookmarksCount || 1000
   },
-  async incognito() {
-    return [{ text: "new incognito tab" }]
+  incognito(op, list) {
+    let url = urlFix(op.strings.join(""))
+    return [{
+      title: "Open in incognito window",
+      text: url,
+      value: url,
+      param: {
+        action: "func",
+        func: this.chrome.createWin,
+        args: [{
+          url,
+          incognito: true,
+          focused: true,
+          state: "maximized"
+        }]
+      }
+    }]
   },
   async history() {
     debug("call history")
-    return (await this.chrome.getHistory()).map(h => ({ text: h.title, value: h.title }))
+    return (await this.chrome.getHistory())
+      .map(h => ({
+        title: h.title, text: h.url, value: h.url,
+        param: {
+          url: h.url
+        }
+      }))
   },
-  bookmarks() {
-    return [{ text: "list bookmarks" }]
+  async bookmarks() {
+    return (await this.chrome.getBookmarks(this.maxBookmarksCount))
+      .map(b => ({
+        title: b.title,
+        text: b.url,
+        value: b.url
+      }))
   },
-  open() {
-    return [{ text: "Open link" }]
+  open(op, list) {
+    return this.new(op, list)
   },
   async tabs() {
     return (await this.chrome.getTabs()).map(tab => ({
@@ -69,24 +124,81 @@ export default decorate({
       }
     }))
   },
-  close() {
-    return [{ text: "close tabs" }]
+  close(op, list: IResult[]) {
+    let tabs = list.filter(item => !!item.param.tabId)
+    return tabs.map(tab => ({
+      title: "Close",
+      text: tab.text,
+      value: tab.value,
+      param: {
+        action: "func",
+        func: this.chrome.closeTab,
+        args: [tab.param.tabId]
+      }
+    }))
   },
   active(op, list: IResult[]) {
     let tabs = list.filter(item => !!item.param.tabId)
+    debug(tabs)
     return tabs.map(tab => ({
       title: "Active",
       text: tab.text,
       value: tab.value,
       param: {
         action: "func",
-        func: this.chrome.activeTab.bind(this.chrome),
+        func: this.chrome.activeTab,
         args: [tab.param.winId, tab.param.tabId]
       }
     }))
   },
-  new() {
-    return [{ text: "new tab" }]
+  async current() {
+    return (await this.chrome.getCurrentTab()).map(tab => ({
+      text: tab.title,
+      value: tab.title,
+      param: {
+        tabId: tab.id,
+        winId: tab.winId
+      }
+    }))
+  },
+  notify(op) {
+    let { title, message } = op
+    return [{
+      title: "Send Notification",
+      text: `${message || "using --message"}`,
+      param: {
+        action: "func",
+        func: this.chrome.notify,
+        args: [title, message]
+      }
+    }]
+  },
+  new(op, list: IResult[]) {
+    if (list.length === 0) {
+      let url = urlFix(op.strings.join(" "))
+      return [{
+        text: `Open ${url} in new tab`,
+        param: {
+          action: "func",
+          func: this.chrome.newTab,
+          args: [{
+            url,
+          }]
+        }
+      }]
+    } else {
+      return list.map(item => ({
+        title: `Open ${item.title}`,
+        text: `Open ${(item.param && item.param.url) || item.value} in new tab`,
+        param: {
+          action: "func",
+          func: this.chrome.newTab,
+          args: [{
+            url: urlFix((item.param && item.param.url) || item.value)
+          }]
+        }
+      }))
+    }
   }
 }, (obj, processor) =>
     async (op, list) => {
@@ -108,6 +220,9 @@ export default decorate({
         return [{
           title: "Chrome not ready",
           text: TIPS
+        }, {
+          title: "https://github.com/OuyangQianba/copilot-chrome",
+          value: "https://github.com/OuyangQianba/copilot-chrome"
         }]
       }
     }
